4b60dadfa04f987f041dce2cbe32de30cab684e5
Sumon Ahmed <bdsumon4u@gmail.com>
2023-08-08 21:01:40 +0600

    Calculator UI

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A new Flutter project.
 
+# Screenshot
+
+![Screenshot](./screenshot.png "Calculator UI")
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -2,10 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-class MyAppState extends ChangeNotifier {
-  
-}
-
+class MyAppState extends ChangeNotifier {}
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -13,12 +10,115 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [],
+      appBar: AppBar(title: const Text('Namer App')),
+      body: Container(
+        color: Colors.blueGrey[100],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  color: Color(0xFFE0E0E0),
+                  child: SizedBox(
+                    width: 340,
+                    height: 70,
+                    child: Text("Display"),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InputButton(inp: "AC"),
+                InputButton(inp: "C"),
+                InputButton(inp: "<"),
+                InputButton(inp: "/"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InputButton(inp: "9"),
+                InputButton(inp: "8"),
+                InputButton(inp: "7"),
+                InputButton(inp: "*"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InputButton(inp: "6"),
+                InputButton(inp: "5"),
+                InputButton(inp: "4"),
+                InputButton(inp: "-"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InputButton(inp: "3"),
+                InputButton(inp: "2"),
+                InputButton(inp: "1"),
+                InputButton(inp: "+"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InputButton(inp: "+/-"),
+                InputButton(inp: "0"),
+                InputButton(inp: "00"),
+                InputButton(inp: "="),
+              ],
+            ),
+            SizedBox(height: 50),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputButton extends StatelessWidget {
+  String inp;
+
+  InputButton({
+    super.key,
+    required this.inp,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: SizedBox(
+        width: 70,
+        height: 70,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+          onPressed: () {},
+          child: Text(
+            inp,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
